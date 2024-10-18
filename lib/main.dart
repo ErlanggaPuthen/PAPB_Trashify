568900868c91a34713365767abd181c5b65357dd
ErlanggaPuthen <11211057@student.itk.ac.id>
2024-10-18 19:17:35 +0800

    ubah isi file main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:trashify_mobile/screens/home_screen.dart';
+import 'package:trashify_mobile/screens/hasil_riwayat_prediksi.dart';
+import 'package:trashify_mobile/screens/profile.dart';
 import 'package:trashify_mobile/register.dart';
-// import './splash_screen.dart';
 import './login.dart';
 
 void main() async {
@@ -22,7 +23,6 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -31,13 +31,64 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: SignInPage(),
+      home: MainScreen(),
       routes: {
-        '/home': (context) => HomeScreen(), // Pastikan HomeScreen sudah di-import
+        '/home': (context) => HomeScreen(),
         '/login': (context) => SignInPage(),
         '/register': (context) => SignUpPage(),
-        
-      },debugShowCheckedModeBanner: false,
+      },
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+  static const List<Widget> _widgetOptions = <Widget>[
+    HomeScreen(),
+    HasilRiwayatPrediksi(),
+    Profile(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Trashify'),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Homescreen',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.history),
+            label: 'Riwayat Prediksi',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.deepPurple,
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
